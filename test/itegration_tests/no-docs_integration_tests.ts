3cd600dfd41e2ca89b547633fa9cdf88038f0fc3
yonadav <yonadav.barilan@gmail.com>
2022-04-06 14:57:35 +0300

    created a crude rest api example with fastify

--- test/itegration_tests/no-docs_integration_tests.ts
+++ test/itegration_tests/no-docs_integration_tests.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-axios.get('http://localhost:3000/nanit').then(r=>console.log({getResponse: r.data}));
-axios.post('http://localhost:3000/nanit', {}).then(r=>console.log({postResponse: r.data}));
-axios.put('http://localhost:3000/nanit/123', {}).then(r=>console.log({putResponse: r.data}));
-axios.delete('http://localhost:3000/nanit/123').then(r=>console.log({deleteResponse: r.data}));
+axios.get('http://localhost:3000/no-docs').then(r=>console.log({getResponse: r.data}));
+axios.post('http://localhost:3000/no-docs', {}).then(r=>console.log({postResponse: r.data}));
+axios.put('http://localhost:3000/no-docs/123', {}).then(r=>console.log({putResponse: r.data}));
+axios.delete('http://localhost:3000/no-docs/123').then(r=>console.log({deleteResponse: r.data}));
